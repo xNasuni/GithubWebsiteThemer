8b0b71d596625bf896e31d12447f626eae3fa2ca
xNasuni <62818119+xNasuni@users.noreply.github.com>
2022-12-14 22:06:38 -0500

    test #1
    testing something

--- githubwebsitethemer.user.js
+++ githubwebsitethemer.user.js
@@ -1,1 +1,13 @@
-console.log('test')
+// ==UserScript==
+// @name Github Website Themer
+// @namespace Mia's Utility Scripts
+// @version 2
+// @updateURL https://github.com/xNasuni/GithubWebsiteThemer/raw/main/githubwebsitethemer.user.js
+// @downloadURL https://github.com/xNasuni/GithubWebsiteThemer/raw/main/githubwebsitethemer.user.js
+// @author Mia
+// @description Changes the main colors of the GitHub website, like all of the blue.
+// @include *github.com/*
+// @grant none
+// ==/UserScript==
+
+alert(1)
